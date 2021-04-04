ecd9ef25eb2e0aaf7fc3f988f7566067b071927f
Jesse Higginson <kadmium@gmail.com>
2021-04-04 18:35:49 +1000

    Added endpoint listen method

--- Kadmium-Udp/IUdpWrapper.cs
+++ Kadmium-Udp/IUdpWrapper.cs
@@ -10,7 +10,7 @@ namespace Kadmium_Udp
 	public interface IUdpWrapper : IDisposable
 	{
 		event EventHandler<UdpReceiveResult> OnPacketReceived;
-		void Listen(string hostname, int port = 0);
+		void Listen(IPEndPoint endPoint);
 		void Listen(int port = 0);
 		Task Send(string hostname, int port, ReadOnlyMemory<byte> packet);
 		void JoinMulticastGroup(IPAddress address);
