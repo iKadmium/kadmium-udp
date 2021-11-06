9a7bd7c65c40b97447e4f5119301c0638a6c9419
Jesse Higginson <kadmium@gmail.com>
2021-11-06 17:41:08 +1100

    Added same test for ipv4

--- Kadmium-Udp.Test/UdpPipelineIntegrationTests.cs
+++ Kadmium-Udp.Test/UdpPipelineIntegrationTests.cs
@@ -139,6 +139,25 @@ namespace Kadmium_Udp.Test
 		}
 
 		[Fact]
+		public async Task Given_AnIPV4Address_When_JoinMultiCastGroupIsCalled_Then_NoExceptionIsThrown()
+		{
+			var hostname = Dns.GetHostName();
+			var addresses = await Dns.GetHostAddressesAsync(hostname);
+			var address = addresses.First(x => x.AddressFamily == AddressFamily.InterNetwork);
+			var remoteAddress = IPAddress.Parse($"239.255.0.1");
+
+			var endpoint = new IPEndPoint(address, 0);
+			byte[] expected = new byte[] { 1, 2, 3, 4 };
+
+			using UdpPipeline sockPipe = new UdpPipeline(AddressFamily.InterNetwork);
+			var pipe = new Pipe();
+
+			var listenTask = sockPipe.ListenAsync(pipe.Writer, endpoint);
+			sockPipe.JoinMulticastGroup(remoteAddress);
+			sockPipe.DropMulticastGroup(remoteAddress);
+		}
+
+		[Fact]
 		public async Task Given_AnIPV6Address_When_JoinMultiCastGroupIsCalled_Then_NoExceptionIsThrown()
 		{
 			var hostname = Dns.GetHostName();
